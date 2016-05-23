f7b1c3a243be2b461cfab0fee74df98db2135376
Manfred Touron <m@42.am>
2016-05-23 17:56:37 +0200

    Configure cli

--- cmd/bolosseum/main.go
+++ cmd/bolosseum/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/moul/bolosseum"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Printf("Bolosseum v%s\n", bolosseum.VERSION)
+	app := cli.NewApp()
+	app.Name = "bolosseum"
+	app.Usage = "colosseum for bots"
+	app.Version = bolosseum.VERSION
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "Start a battle",
+			Action: func(c *cli.Context) error {
+				args := c.Args()
+				if len(args) < 1 {
+					return cli.NewExitError("You need to specify the game", -1)
+				}
+
+				game := args[0]
+				logrus.Warnf("Initializing game %q", game)
+
+				// temporarily hardcoded check
+				if len(args) < 3 {
+					return cli.NewExitError("You need to specify two bots", -1)
+				}
+				bots := args[1:3]
+				logrus.Warnf("Bots: %q", bots)
+
+				return nil
+			},
+		},
+	}
+	app.Run(os.Args)
 }
